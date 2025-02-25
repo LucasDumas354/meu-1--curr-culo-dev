fd50c812fc7d912cc7529a0961430a8e2c845c6c
Lucas Nunes Dumas <128542655+wonalucard354@users.noreply.github.com>
2025-02-25 14:10:59 -0300

    meu primeiro cartão de visita como dev

---
+++ curriculo.js
@@ -0,0 +1,17 @@
+// Aguarda o carregamento completo da página
+//addEventListener('load', function() {
+    // Seleciona o contêiner
+  //  const container = document.querySelector('.container')
+    
+    // Adiciona a classe 'visible' para ativar a transição
+ //   container.classList.add('visible')
+//})
+
+// Aguarda o carregamento completo da página
+//addEventListener('load', function() {
+    // Seleciona a div
+  //  const box = document.getElementById('animatedBox')
+    
+    // Adiciona a classe 'visible' para ativar a animação
+   // box.classList.add('visible')
+//})
